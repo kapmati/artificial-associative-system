640ef7eed146a3b31cbd38d96fff256ad20cb537
Mateusz Kapron <kapmat77@gmail.com>
2016-09-21 20:32:38 +0200

    Initial commit

---
+++ src/main/java/pl/kapmat/Application.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Kapmat on 2016-09-21.
+ */
+package pl.kapmat;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/main/java/pl/kapmat/model/Sentence.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Kapmat on 2016-09-21.
+ */
+package pl.kapmat.model;
+
+public class Sentence {
+}
